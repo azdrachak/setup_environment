3847b0538b23cd740835bdd44861cfb754f351bb
Aliaksandr Zdrachak <iscander.drake@gmail.com>
2017-02-24 16:33:21 +0300

    Add different browser initialisation

--- src/test/java/LitecartLogin.java
+++ src/test/java/LitecartLogin.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,15 +17,18 @@ public class LitecartLogin {
 
     @Before
     public void setUp() {
-        this.driver = new ChromeDriver();
+//        this.driver = new ChromeDriver();
+//        this.driver = new FirefoxDriver();
+        this.driver = new InternetExplorerDriver();
+//        this.driver = new PhantomJSDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void loginTest() {
-        driver.get("http://localhost:8080/litecart/admin/");
-
+//        driver.get("http://localhost:8080/litecart/admin/");
+        driver.navigate().to("http://localhost:8080/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -12,25 +13,30 @@ public class MyFirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    @Before
+/*    @Before
     public void start() {
-        this.driver = new ChromeDriver();
+//        this.driver = new ChromeDriver();
+        this.driver = new InternetExplorerDriver();
         this.driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
-    }
+    }*/
 
     @Test
     public void myFirstTest() {
+        driver = new InternetExplorerDriver();
+
         driver.get("https://www.google.com/ncr");
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("btnG")).click();
         wait.until(titleIs("webdriver - Google Search"));
+
+        driver.quit();
     }
 
-    @After
+/*    @After
     public void stop() {
         driver.quit();
         driver = null;
-    }
+    }*/
 
 }
