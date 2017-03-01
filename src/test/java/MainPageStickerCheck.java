b906dc968d816c4300bbfd4de470050abb48c14d
Aliaksandr Zdrachak <iscander.drake@gmail.com>
2017-03-01 23:20:03 +0300

    Add test to check if all ducks have stickers

---
+++ src/test/java/MainPageStickerCheck.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MainPageStickerCheck {
+    private WebDriver driver;
+    private final String mainPage = "http://localhost:8080/litecart/en/";
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void duckyStickersTest() {
+        driver.navigate().to(mainPage);
+        List<WebElement> ducks = driver.findElements(By.cssSelector(".image-wrapper"));
+        for (WebElement duck : ducks) {
+            Assert.assertTrue(duck.findElements(By.cssSelector("div[class^=sticker]")).size() > 0);
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
